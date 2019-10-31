2b733c133ad35c34d60bb32babaecde409818149
Younghooon Kim <yhkim1031@gmail.com>
2019-10-31 17:30:00 +0900

    first

---
+++ spring-demo-one/src/com/springdemo/TrackCoach.java
@@ -0,0 +1,27 @@
+package com.springdemo;
+
+public class TrackCoach implements Coach {
+
+private FortuneService fortuneService;
+	
+	public TrackCoach() {}
+
+	public TrackCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	
+	
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Run a hard 5k";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		
+		return "Just Do it: "+fortuneService.getFortune();
+	}
+
+}
