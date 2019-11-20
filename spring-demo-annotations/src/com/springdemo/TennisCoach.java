75ccc4da08ac256572136669168b5062f4ff0e98
Younghooon Kim <yhkim1031@gmail.com>
2019-11-20 19:27:48 +0900

    annotation demo

--- spring-demo-annotations/src/com/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/springdemo/TennisCoach.java
@@ -1,22 +1,49 @@
 package com.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
-
+	
+//	@Autowired
+//	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
 	
+	@Value("${foo.email}")
+	private String email;
+	
+	@Value("${foo.team}")
+	private String team;
+	
+	
 	// define a default constructor
+	
 	public TennisCoach() {
 		System.out.println(">> TennisCoach: inside default constructor");
+	}
+	
+	// define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyStartupStuff");
+	}
+	
+	// define my destroy method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyCleanupStuff");
 	}
 	
 	// define a setter method
 	
 	@Autowired
-	public void setFortuneService(FortuneService theFortuneSerivce) {
+	public void setFortuneService(@Qualifier("randomFortuneService") FortuneService theFortuneSerivce) {
 		System.out.println(">> TennisCoach: inside setFortuneService() method");
 		this.fortuneService = theFortuneSerivce;
 	}
@@ -33,7 +60,7 @@ public class TennisCoach implements Coach {
 	
 	/*
 	@Autowired
-	public TennisCoach(FortuneService theFortuneSerivce) {
+	public TennisCoach(@Qualifier("randomFortuneService") FortuneService theFortuneSerivce) {
 		fortuneService=theFortuneSerivce;
 	}
 	*/
@@ -41,7 +68,7 @@ public class TennisCoach implements Coach {
 	
 	@Override
 	public String getDailyWorkout() {
-		return "Practice your backhand volley";
+		return "Practice your backhand volley"+" "+email+" "+team;
 	}
 
 	@Override
